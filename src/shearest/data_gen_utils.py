f551383a13c4fbb5d5b22ba8f8120644f77b6d03
CentofantiEze <ezecentofanti@gmail.com>
2025-09-22 11:30:01 +0200

    Include option to use deepshape data.

--- src/shearest/data_gen_utils.py
+++ src/shearest/data_gen_utils.py
@@ -1,94 +1,158 @@
-import numpy as np
+from functools import partial
+
+import galsim as gs
+import h5py
 import jax.numpy as jnp
 import jax_galsim as galsim
-import galsim as gs
-
-from functools import partial
+import numpy as np
 import numpyro
 import numpyro.distributions as dist
 
-from .func_utils import complex_2_stack
-from .func_utils import to_unit_disk
+from .func_utils import complex_2_stack, to_unit_disk
+
 
 def draw_exp_profile(hlr, flux, e1, e2, g1, g2, uv_pos, Npx, pixel_scale):
     gal = galsim.Exponential(half_light_radius=hlr, flux=flux)
-    
+
     # intrinsic ellipticity
     gal = gal.shear(e1=e1, e2=e2)
 
     # cosmic shear
-    gal = gal.shear(g1=g1,g2=g2)
+    gal = gal.shear(g1=g1, g2=g2)
 
     # Convert to Fourier space
     gal_kimage = gal.drawKImage(nx=Npx, ny=Npx, scale=2 * jnp.pi / (Npx * pixel_scale))
-    
+
     # Get array
-    gal_kimage = gal_kimage.array 
+    gal_kimage = gal_kimage.array
 
     # Sample the visibilities
     vis = gal_kimage[uv_pos]
-    
+
     return complex_2_stack(vis)
+
 
 def draw_sersic_profile(n, hlr, flux, e1, e2, g1, g2, uv_pos, Npx, pixel_scale):
     # n = np.random.rand() * 0. + 4. # Exponential profile, n=1
     gal = gs.Sersic(n=n, half_light_radius=hlr, flux=flux)
-    
+
     # intrinsic ellipticity
     gal = gal.shear(e1=e1, e2=e2)
 
     # cosmic shear
-    gal = gal.shear(g1=g1,g2=g2)
+    gal = gal.shear(g1=g1, g2=g2)
 
     # Convert to Fourier space
     gal_kimage = gal.drawKImage(nx=Npx, ny=Npx, scale=2 * np.pi / (Npx * pixel_scale))
-    
+
     # Get array
-    gal_kimage = gal_kimage.array 
-    
+    gal_kimage = gal_kimage.array
+
     # Sample the visibilities
     vis = gal_kimage[uv_pos]
 
     return complex_2_stack(vis)
 
-def gen_sersic_profile(Ngal=None, Npx=None, pixel_scale=None, uv_pos=None, noise_uv=None, params_dir=None,
-            ell_sigma=None,
-            ell_scale=None,
-            g_sigma=None,
-            g_scale=None,
-            n=1.):
 
-    # define parameters and their prior
-    params = np.load(params_dir, allow_pickle=True)[()]
+def sample_sersic_params(
+    Ngal=None, TRECS_fit_dir=None, ell_scale=None, n=1.0, deepshape_dataset_dir=None
+):
 
-    u = jnp.ones((Ngal,)) # sampling galaxies all at once
+    if TRECS_fit_dir is not None:
+        params = np.load(TRECS_fit_dir, allow_pickle=True)[()]
+        u = jnp.ones((Ngal,))  # sampling galaxies all at once
 
-    hlr_fit = params['beta_fit_hlr']
-    hlr = numpyro.sample("hlr", dist.Beta(hlr_fit['a'], hlr_fit['b']), sample_shape=(Ngal,)) * hlr_fit['scale'] + hlr_fit['loc']
+        hlr_fit = params["beta_fit_hlr"]
+        hlr = (
+            numpyro.sample(
+                "hlr", dist.Beta(hlr_fit["a"], hlr_fit["b"]), sample_shape=(Ngal,)
+            )
+            * hlr_fit["scale"]
+            + hlr_fit["loc"]
+        )
 
-    
-    flux_fit = params['beta_fit_flux']
-    flux = numpyro.sample("flux", dist.Beta(flux_fit['a'], flux_fit['b']), sample_shape=(Ngal,)) * flux_fit['scale'] + flux_fit['loc']
- 
-    e1 = numpyro.sample("e1", dist.Normal(0.*u, ell_sigma*u))/ell_sigma * ell_scale
-    e2 = numpyro.sample("e2", dist.Normal(0.*u, ell_sigma*u))/ell_sigma * ell_scale
+        flux_fit = params["beta_fit_flux"]
+        flux = (
+            numpyro.sample(
+                "flux", dist.Beta(flux_fit["a"], flux_fit["b"]), sample_shape=(Ngal,)
+            )
+            * flux_fit["scale"]
+            + flux_fit["loc"]
+        )
 
-    # assuming constant shear across galaxies
-    g1 = numpyro.sample("g1", dist.Normal(jnp.zeros((1,)), g_sigma*jnp.ones((1,))))*g_scale/g_sigma
-    g2 = numpyro.sample("g2", dist.Normal(jnp.zeros((1,)), g_sigma*jnp.ones((1,))))*g_scale/g_sigma
+        e1 = numpyro.sample("e1", dist.Normal(0.0 * u, 1.0 * u)) * ell_scale
+        e2 = numpyro.sample("e2", dist.Normal(0.0 * u, 1.0 * u)) * ell_scale
 
-    # clipping undefined e and g values
-    e = jnp.stack([e1, e2], 0)
-    e = to_unit_disk(e)
+        # clipping undefined e and g values
+        e = jnp.stack([e1, e2], 0)
+        e = to_unit_disk(e)
 
-    g = jnp.repeat(jnp.stack([g1, g2], 0), Ngal, -1)
-    g = to_unit_disk(g)
+        n = jnp.ones((Ngal,)) * n  # Exponential profile, n=1
+    elif deepshape_dataset_dir is not None:
+        # Load the dataset
+        with h5py.File(deepshape_dataset_dir, "r") as f:
+            print("Keys in the dataset:", list(f.keys()))
+            columns = list(f.keys())
+            data = {col: f[col][:] for col in columns}
+        # Load parameters
+        hlr = data["HLR"][:Ngal]
+        flux = data["Flux"][:Ngal]
+        e = data["input"][:Ngal]
+        n = data["Sersic_index"][:Ngal]
 
+    return hlr, flux, e, n
+
+
+def gen_sersic_profile(
+    Ngal=None,
+    Npx=None,
+    pixel_scale=None,
+    uv_pos=None,
+    noise_uv=None,
+    TRECS_fit_dir=None,
+    deepshape_dataset_dir=None,
+    ell_scale=None,
+    g1=None,
+    g2=None,
+    n=1.0,
+):
+
+    hlr, flux, e, n = sample_sersic_params(
+        Ngal=Ngal,
+        TRECS_fit_dir=TRECS_fit_dir,
+        ell_scale=ell_scale,
+        n=n,
+        deepshape_dataset_dir=deepshape_dataset_dir,
+    )
+
+    u = jnp.ones((Ngal,))  # Sheared shear for all galaxies
+    g_1 = u * g1
+    g_2 = u * g2
     # generate galaxy image
-    draw = partial(draw_sersic_profile, n=n, uv_pos=uv_pos, Npx=Npx, pixel_scale=pixel_scale)
-    im_gal = jnp.array([draw(hlr=hlr[i], flux=flux[i], e1=e[0][i], e2=e[1][i], g1=g[0][i], g2=g[1][i]) for i in range(Ngal)])
-    data_params = {"n": n, "hlr": hlr, "flux": flux, "e1": e[0], "e2": e[1], "g1": g[0], "g2": g[1]}
+    draw = partial(draw_sersic_profile, uv_pos=uv_pos, Npx=Npx, pixel_scale=pixel_scale)
+    im_gal = jnp.array(
+        [
+            draw(
+                n=n[i],
+                hlr=hlr[i],
+                flux=flux[i],
+                e1=e[0][i],
+                e2=e[1][i],
+                g1=g_1[i],
+                g2=g_2[i],
+            )
+            for i in range(Ngal)
+        ]
+    )
+    data_params = {
+        "n": n,
+        "hlr": hlr,
+        "flux": flux,
+        "e1": e[0],
+        "e2": e[1],
+        "g1": g_1,
+        "g2": g_2,
+    }
 
     # add Gaussian noise
     return numpyro.sample("obs", dist.Normal(im_gal, noise_uv)), data_params
-
--- src/shearest/model_utils.py
+++ src/shearest/model_utils.py
@@ -1,47 +1,70 @@
-import numpy as np
+from functools import partial
+
 import jax
 import jax.numpy as jnp
+import numpy as np
 import numpyro
 import numpyro.distributions as dist
-
-from functools import partial
 
 from .data_gen_utils import draw_exp_profile
 from .func_utils import to_unit_disk
 
 
-#@partial(jax.jit, static_argnums=(0,1,2,3,4))
-def model_fn(Ngal=None, Npx=None, pixel_scale=None, uv_pos=None, noise_uv=None, obs=None,
-            ell_sigma=None,
-            ell_scale=None,
-            g_sigma=None,
-            g_scale=None,
-            hlr_sigma=None,
-            hlr_max=None,
-            hlr_min=None,
-            flux_sigma=None,
-            flux_max=None,
-            flux_min=None):
+# @partial(jax.jit, static_argnums=(0,1,2,3,4))
+def model_fn(
+    Ngal=None,
+    Npx=None,
+    pixel_scale=None,
+    uv_pos=None,
+    noise_uv=None,
+    obs=None,
+    ell_sigma=None,
+    ell_scale=None,
+    g_sigma=None,
+    g_scale=None,
+    hlr_sigma=None,
+    hlr_max=None,
+    hlr_min=None,
+    flux_sigma=None,
+    flux_max=None,
+    flux_min=None,
+):
 
-    u = jnp.ones((Ngal,)) # sampling galaxies all at once
+    u = jnp.ones((Ngal,))  # sampling galaxies all at once
 
     # hlr
     # hlr = jax.nn.softplus((numpyro.sample("hlr", dist.Normal(0.*u, hlr_sigma*u))/hlr_sigma + hlr_offset)) * hlr_scale + hlr_min
-    hlr_z = numpyro.sample("hlr", dist.Normal(0.*u, hlr_sigma*u))
-    hlr = hlr_min + jax.nn.sigmoid(hlr_z/hlr_sigma) * (hlr_max - hlr_min)
+    hlr_z = numpyro.sample("hlr", dist.Normal(0.0 * u, hlr_sigma * u))
+    hlr = hlr_min + jax.nn.sigmoid(hlr_z / hlr_sigma) * (hlr_max - hlr_min)
 
     # flux
     # flux = jax.nn.softplus((numpyro.sample("flux", dist.Normal(0.*u, flux_sigma*u))/flux_sigma + flux_offset)) * flux_scale + flux_min
-    flux_z = numpyro.sample("flux", dist.Normal(0.*u, flux_sigma*u))
-    flux = flux_min + jax.nn.sigmoid(flux_z/flux_sigma) * (flux_max - flux_min)
+    flux_z = numpyro.sample("flux", dist.Normal(0.0 * u, flux_sigma * u))
+    flux = flux_min + jax.nn.sigmoid(flux_z / flux_sigma) * (flux_max - flux_min)
 
     # ellipticity
-    e1 = numpyro.sample("e1", dist.Normal(0.*u, ell_sigma*u))/ell_sigma * ell_scale
-    e2 = numpyro.sample("e2", dist.Normal(0.*u, ell_sigma*u))/ell_sigma * ell_scale
+    e1 = (
+        numpyro.sample("e1", dist.Normal(0.0 * u, ell_sigma * u))
+        / ell_sigma
+        * ell_scale
+    )
+    e2 = (
+        numpyro.sample("e2", dist.Normal(0.0 * u, ell_sigma * u))
+        / ell_sigma
+        * ell_scale
+    )
 
     # assuming constant shear across galaxies
-    g1 = numpyro.sample("g1", dist.Normal(jnp.zeros((1,)), g_sigma*jnp.ones((1,))))*g_scale/g_sigma
-    g2 = numpyro.sample("g2", dist.Normal(jnp.zeros((1,)), g_sigma*jnp.ones((1,))))*g_scale/g_sigma
+    g1 = (
+        numpyro.sample("g1", dist.Normal(jnp.zeros((1,)), g_sigma * jnp.ones((1,))))
+        * g_scale
+        / g_sigma
+    )
+    g2 = (
+        numpyro.sample("g2", dist.Normal(jnp.zeros((1,)), g_sigma * jnp.ones((1,))))
+        * g_scale
+        / g_sigma
+    )
 
     # clipping undefined e and g values
     e = jnp.stack([e1, e2], 0)
@@ -51,11 +74,13 @@ def model_fn(Ngal=None, Npx=None, pixel_scale=None, uv_pos=None, noise_uv=None, 
     g = to_unit_disk(g)
 
     draw = partial(draw_exp_profile, uv_pos=uv_pos, Npx=Npx, pixel_scale=pixel_scale)
-    im_gal = jax.vmap(draw)(hlr=hlr,
-                            flux=flux, 
-                            e1=e[0], 
-                            e2=e[1],
-                            g1=g[0], 
-                            g2=g[1],)
-    
+    im_gal = jax.vmap(draw)(
+        hlr=hlr,
+        flux=flux,
+        e1=e[0],
+        e2=e[1],
+        g1=g[0],
+        g2=g[1],
+    )
+
     return numpyro.sample("obs", dist.Normal(im_gal, noise_uv), obs=obs)
